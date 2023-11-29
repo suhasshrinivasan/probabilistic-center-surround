0e9a41d00a1e0328a02b445d2e2eac5ace43fc54
Suhas Shrinivasan <suhasshrinivasan@gmail.com>
2023-11-29 14:10:31 +0100

    Update (minor)

--- setup.py
+++ setup.py
@@ -3,5 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name="probcs",
     version="0.0.0",
-    packages=find_packages(),
+    packages=find_packages(
+        include=["experiments", "models", "routines", "utils"]
+    ),
 )
