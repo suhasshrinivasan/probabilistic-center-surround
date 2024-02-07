ec30b72e6b46b3ffb75e961c58ecfc18af15f2f2
Suhas Shrinivasan <suhasshrinivasan@gmail.com>
2024-02-07 16:47:39 +0100

    Add DJ running code for binary experiment

---
+++ experiments/dj_experiments/dj_binary_experiment.py
@@ -0,0 +1,47 @@
+import itertools as it
+from collections import OrderedDict
+
+from probcs.datajoint.exc_tables import BernoulliConfig, BernoulliResult
+
+from probcs.utils.utils import make_hash
+
+configs = OrderedDict(
+    seed=[42],
+    g_dim=[5],
+    g_prob=[1 / 10, 1 / 5, 1 / 3, 1 / 2, 0.8],
+    i_sigma=[30, 20, 10, 5],
+    patterns_offset=[0],
+    offset_x_1=[0],
+    offset_x_2=[1],
+    scale_x=[0.1],
+    exponent_x=[3],
+    n_tune=[300],
+    n_draws=[300],
+    n_chains=[4],
+    n_cores=[4],
+)
+
+# # test config for debugging
+# configs = OrderedDict(
+#     seed=[42],
+#     g_dim=[2],
+#     g_prob=[1 / 2],
+#     x_sigma=[0.1],
+#     i_sigma=[1],
+#     patterns_offset=[0],
+#     n_tune=[5],
+#     n_draws=[5],
+#     n_chains=[2],
+#     n_cores=[2],
+# )
+
+
+config_list = []
+for values in it.product(*configs.values()):
+    config = {key: value for key, value in zip(configs.keys(), values)}
+    config["config_id"] = make_hash(config)
+    config_list.append(config)
+
+BernoulliConfig.insert(config_list, skip_duplicates=True)
+# CenterExperimentResult.populate()
+BernoulliResult.populate(reserve_jobs=True, order="random")
